884c8ffe3a92f088afb96626dc08e68ab5776be2
Ankesh S <ankesh.singh@vicara.co.in>
2023-01-18 19:26:19 +0400

    Option to enter dir location

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use http_body_util::Full;
 use hyper::service::service_fn;
 use hyper::{Request, Response, Result, StatusCode};
 
-static INDEX: &str = "./";
+// static INDEX: &str = "./";
 static NOTFOUND: &[u8] = b"Not Found";
 
 #[tokio::main]
@@ -24,14 +24,27 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
             .takes_value(true)
             .required(false)
             .help("Sets the port number"),
+        ) 
+        .arg(
+            Arg::new("file")
+            .short('f')
+            .long("file")
+            .value_name("FILE-NAME")
+            .takes_value(true)
+            .help("File to be servered")
         )
         .get_matches();
     
     let mut file_port = String::from("8000");
+    let mut file_name = String::from("index.html");
 
     if let Some(i) = matches.value_of("port"){
         file_port.replace_range(.., i);
     }
+    if let Some(j) = matches.value_of("file"){
+        file_name.replace_range(.., j);
+    }
+
 
     // Starting server
     println!("Static file server is running on address {}", file_port);
@@ -40,10 +53,11 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let addr = SocketAddr::from(([127,0,0,1], file_port.parse::<u16>().unwrap()));
     let listener = TcpListener::bind(addr).await.unwrap();
     loop{
+        let file = file_name.clone();
         let (stream, _) = listener.accept().await.unwrap();
         tokio::task::spawn(async move{
             if let Err(err) = 
-                http1::Builder::new().serve_connection(stream, service_fn(handle_connection)).await {
+                http1::Builder::new().serve_connection(stream, service_fn(move |req| handle_connection(req, file.to_string()))).await {
                         eprintln!("Error serving connection: {:?}", err);
                     }
         });
@@ -51,8 +65,8 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
 }
 
-async fn handle_connection(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>> {
-    let url = INDEX.to_string();
+async fn handle_connection(req: Request<hyper::body::Incoming>, file: String) -> Result<Response<Full<Bytes>>> {
+    let url = &file;
     let path = req.uri().path().to_string();
     let fn_path = format!("{}{}",url,path);
     println!("You are currently viewing file: {} {}", url, path);
